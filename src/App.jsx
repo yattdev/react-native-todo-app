bdfe1ec8616bc8311df1355ebd3d0dba7243c93d
yattdev <yattdeveloper@gmail.com>
2021-12-29 16:56:35 +0100

    Update App.jsx to add tabBarIcon option and hide header text

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import Home from './screens/Home';
 
 const Tab = createBottomTabNavigator();
@@ -11,7 +11,23 @@ const App = () => {
   return (
     <>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            header: () => null,
+            tabBarIcon: ({focused, size, color}) => {
+              let iconName;
+              if (route.name === 'Home') {
+                iconName = 'home';
+                size = focused ? 25 : 20;
+                color = focused ? '#f0f' : '#555';
+              }
+              return (
+                <>
+                  <Icon name={iconName} size={25} color={color} />
+                </>
+              );
+            },
+          })}>
           <Tab.Screen name="Home" component={Home} />
         </Tab.Navigator>
       </NavigationContainer>
