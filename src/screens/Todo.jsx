469ee57f5afa263577246c3ca227e9d071ff191a
yattdev <yattdeveloper@gmail.com>
2021-12-31 11:05:13 +0100

    Create a new screen named Todo where ongoing todo tasks

---
+++ src/screens/Todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Todo = () => {
+  return (
+    <>
+      <View>
+        <Text>TODO Sreen</Text>{' '}
+      </View>
+    </>
+  );
+};
+
+export default Todo;
