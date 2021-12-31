9c7ab8998b383815f1f65d45af597badf6101a75
yattdev <yattdeveloper@gmail.com>
2021-12-31 13:09:30 +0100

    Update redux actions file to define actions setTaskID to open Task screen

--- src/redux/actions.jsx
+++ src/redux/actions.jsx
@@ -1,8 +1,17 @@
 export const SET_TASKS = 'SET_TASKS';
+export const SET_TASKS_ID = 'SET_TASKS_ID';
 
+// Action to set tasks through redux
 export const setTasks = tasks => dispatch => {
   dispatch({
     type: SET_TASKS,
     payload: tasks,
+  });
+};
+
+export const setTaskID = taskID => dispatch => {
+  dispatch({
+    type: SET_TASKS_ID,
+    payload: taskID,
   });
 };
