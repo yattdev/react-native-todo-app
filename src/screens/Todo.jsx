45aa365e2d9198d8e8c00355fe0c74402e22ab57
yattdev <yattdeveloper@gmail.com>
2021-12-31 12:29:33 +0100

    Update Todo screen to add blue button to add task

--- src/screens/Todo.jsx
+++ src/screens/Todo.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const Todo = () => {
   return (
     <>
-      <View>
-        <Text>TODO Sreen</Text>{' '}
+      <View style={styles.body}>
+        <TouchableOpacity style={styles.button}>
+          <Icon name="plus" size={20} color="#ffffff" />
+        </TouchableOpacity>
       </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+  },
+  button: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#0080ff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    elevation: 10,
+  },
+});
 
 export default Todo;
