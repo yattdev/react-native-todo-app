9a626c9c5337eb12c16bd7f857c2336f8d5ae47c
yattdev <yattdeveloper@gmail.com>
2021-12-31 11:04:13 +0100

    Create new screen named Done where to display the done tasks

---
+++ src/screens/Done.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Done = () => {
+  return (
+    <>
+      <View>
+        <Text>Done Sreen</Text>
+      </View>
+    </>
+  );
+};
+
+export default Done;
