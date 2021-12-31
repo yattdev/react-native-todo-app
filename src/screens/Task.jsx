b0f786eaac918817041250508ad16d54ac54381c
yattdev <yattdeveloper@gmail.com>
2021-12-31 12:53:34 +0100

    Update Task screen:
    - To add Textinput title and description then a button to save task

--- src/screens/Task.jsx
+++ src/screens/Task.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TextInput} from 'react-native';
+import MyButton from '../utils/MyButton';
 
 const Task = () => {
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
   return (
     <>
       <View style={styles.body}>
-        <Text>Task Sreen</Text>
+        <TextInput
+          onChangeText={value => setTitle(value)}
+          style={styles.input}
+          placeholder="Title"
+        />
+        <TextInput
+          onChangeText={value => setDesc(value)}
+          style={styles.input}
+          placeholder="Description"
+          multiline
+        />
+        <MyButton
+          buttonText="Save task"
+          buttonColor="#1ebf00"
+          buttonStyle={styles.button}
+        />
       </View>
     </>
   );
@@ -16,6 +34,22 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     padding: 10,
+  },
+
+  input: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#bbbbbb',
+    borderRadius: 10,
+    backgroundColor: '#ffffff',
+    textAlign: 'left',
+    fontSize: 20,
+    margin: 10,
+    paddingHorizontal: 10,
+  },
+
+  button: {
+    width: '100%',
   },
 });
 
