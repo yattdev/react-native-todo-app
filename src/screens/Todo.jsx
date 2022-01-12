6c10b74f75174c15e975a2064aa44d0e68b91e58
yattdev <yattdeveloper@gmail.com>
2022-01-12 12:11:16 +0100

    UPdate Todo.jsx to get and set tasks to AsyncStorae

--- src/screens/Todo.jsx
+++ src/screens/Todo.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import {useSelector, useDispatch} from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {setTasks, setTaskID} from '../redux/actions';
 
 const Todo = ({navigation}) => {
+  const {tasks} = useSelector(state => state.taskReducer);
+  const dispatch = useDispatch();
+
+  const getTasks = async () => {
+    try {
+      await AsyncStorage.getItem('Tasks')
+        .then(AllTask => {
+          const parsedTasks = JSON.parse(AllTask);
+          if (parsedTasks && typeof parsedTasks === 'object') {
+            dispatch(setTasks(parsedTasks));
+          }
+        })
+        .catch(err => console.log(err));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <View style={styles.body}>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('Task')}>
+          onPress={() => {
+            dispatch(setTaskID(tasks.length + 1));
+            navigation.navigate('AllTask');
+          }}>
           <Icon name="plus" size={20} color="#ffffff" />
         </TouchableOpacity>
       </View>
