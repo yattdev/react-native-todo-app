532102141c358a2d2a01234967248e82161bb24b
yattdev <yattdeveloper@gmail.com>
2022-01-12 12:07:10 +0100

    Update reducers to add case for SET_TASKS_ID

--- src/redux/reducers.jsx
+++ src/redux/reducers.jsx
@@ -9,6 +9,8 @@ const taskReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_TASKS:
       return {...state, tasks: action.payload};
+    case SET_TASKS_ID:
+      return {...state, taskID: action.payload};
     default:
       return state;
   }
